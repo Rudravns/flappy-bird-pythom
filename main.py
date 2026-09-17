20c6468b13774230e56c31c2b16950131209329b
kumar1272 <kumar1272@H-260-27>
2026-09-17 09:51:03 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+def start():
+    pass
+
+"""
+requirements:
+    EXACT CLONE OF FLAPPY BIRD
+    USE BASIC SHAPES (CIRCLES AND RECTANGLES)
+    ONE EXTRA THING needs to be added 
+
+"""
---
+++ pipe.py
@@ -0,0 +1,12 @@
+import pygame
+import random 
+
+class pipe:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
